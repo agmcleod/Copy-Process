4a2e27e85a03b3667d6005a411ce048f038dc7a1
Aaron McLeod <sircoolguy99@gmail.com>
2011-02-13 21:38:15 -0500

    removing old files

--- lib/content_row.rb
+++
@@ -1,9 +0,0 @@
-class ContentRow
-  attr_reader(:type_name, :content, :kw)
-  
-  def initialize(content, type_name, kw)
-    @type_name = type_name
-    @content = content
-    @kw = kw
-  end
-end
