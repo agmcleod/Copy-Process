6778f8653066e56b41c3afe8534c8858a23edad7
Aaron McLeod <sircoolguy99@gmail.com>
2011-09-28 13:36:36 -0400

    cucumber tests passing. Was missing the press a button step

--- app/controllers/search_and_replace_controller.rb
+++ app/controllers/search_and_replace_controller.rb
@@ -6,9 +6,18 @@ class SearchAndReplaceController < ApplicationController
   def create
     @site = Site.find(params[:site_id])
     if params[:search].blank?
-      redirect_to new_site_search_and_replace_url(@site), notice: 'Content not found.'
+      nothing_found(@site.id)
     else
       @element_results = Element.search_by_content(@site.id, params)
+      if @element_results.size == 0
+        nothing_found(@site.id)
+      end
     end
+  end
+  
+  private
+  
+  def nothing_found(site)
+    redirect_to new_site_search_and_replace_url(site), notice: 'Content not found.'
   end
 end
--- features/search_and_replace.feature
+++ features/search_and_replace.feature
@@ -9,6 +9,7 @@ Feature: Search and replace
     And I follow "Search/Replace"
     And I fill in "search" with "Recycling"
     And I check "case_sensitive"
+    And I press "Search"
     Then I should see 4 ".etname" tags
     
   Scenario: Enters in an unavailable search term
@@ -16,4 +17,5 @@ Feature: Search and replace
     When I compile documents
     And I follow "Search/Replace"
     And I fill in "search" with "sdfhjusehnf"
+    And I press "Search"
     Then I should see "Content not found."
