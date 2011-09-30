1b38048b599bd6dc9dc6b47c32baa87091e31a92
Aaron McLeod <sircoolguy99@gmail.com>
2011-09-30 11:27:46 -0400

    one last route change

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ CopyProcess::Application.routes.draw do
     resource :search_and_replace, :only => [:new, :create], :controller => 'search_and_replace'
   end
   
-  root to: 'sites#index'
+  root :to => 'sites#index'
   
   match '/compile_documents/:id' => 'sites#compile_documents', :as => :compile_documents, :via => :post
   match '/export_as_csv/:id' => 'sites#export_csv', :as => :export_csv, :via => :get
