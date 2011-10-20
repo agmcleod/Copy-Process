10dd3f046140e43a2b151c5c900b4a003da87378
Aaron McLeod <sircoolguy99@gmail.com>
2011-10-20 14:55:52 -0400

    Closes #6

--- app/models/document.rb
+++ app/models/document.rb
@@ -55,8 +55,16 @@ class Document < ActiveRecord::Base
   
   def document_is_in_valid_format
     p = CopyProcess::Processor.new
-    unless p.contains_valid_headers(self.content)
-      errors.add(:content, "Contents must be in valid format")
+    if !p.contains_valid_headers(self.content)
+      errors.add(:content, "Headers must be in valid format")
+    else
+      headers = content.split(/\n/)[1..3].join(' - ').gsub(/\/\*|\\\*/,'')
+      site_documents = Document.where(site_id: self.site_id)
+      site_documents.each do |doc|
+        if doc.name == headers
+          errors.add(:content, "headers are not unique.")
+        end
+      end
     end
   end
   
