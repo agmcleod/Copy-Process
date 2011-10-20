3b1cdf0288c13b2d48f64280cf7a97fa09be79bc
Aaron McLeod <sircoolguy99@gmail.com>
2011-10-20 12:39:33 -0400

    added db files back

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+if User.find_by_email('dacgroupdigital@gmail.com').nil?
+  User.create!(:email => 'dacgroupdigital@gmail.com', :password => '8JxD4yXTPoFUfF', :password_confirmation => '8JxD4yXTPoFUfF')
+end
