fe8fc1015a679a4fac020aa5a7bef96bf2282ef7
Aaron McLeod <sircoolguy99@gmail.com>
2011-12-07 15:03:39 -0500

    changes to notes

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,17 +1,17 @@
 module DocumentsHelper
-  def content_with_notes(document)
-    content = h(document.content)
+  def content_with_notes(version)
+    content = h(version.content)
     offset = 0
     
     notes = []
     
-    document.notes.each do |note|
+    version.notes.each do |note|
       tag = "<span class=\"to_change\" id=\"sel_#{note.id}\">"
       close_tag = '</span>'
       
       content.insert(note.start_character + offset, tag)
       offset += tag.size
-      content.insert(note.end_character + offset + 1, close_tag)
+      content.insert(note.end_character + offset, close_tag)
       offset += close_tag.size
     end
     
